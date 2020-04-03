971d9dd2f20260bfa474a8faf519a00dfec9610e
Dante L'Erario <dantelerario@gmail.com>
2020-04-03 16:41:47 +0200

    snack 1 e 2 done

---
+++ assets/js/main.js
@@ -0,0 +1,93 @@
+// JSnack
+
+// Snack 1: L’utente inserisce due parole in successione, con due prompt. Il software stampa la parola più lunga. Utilizza una funzione per determinare e ritornare la parola più lunga.
+
+var btnSnack1 = document.getElementById('startSnack1');
+
+btnSnack1.addEventListener('click',
+  function () {
+
+    var parola1 = document.getElementById('addParola1').value;
+    var parola2 = document.getElementById('addParola2').value;
+
+      if ( parola1.length > parola2.length ) {
+        parolaLunga = 'La parola più lunga è ' + parola1;
+        document.getElementById('risultatoSnack1').innerHTML = parolaLunga;
+      } else if ( parola1.length < parola2.length ) {
+        parolaLunga = 'La parola più lunga è ' + parola2;
+        document.getElementById('risultatoSnack1').innerHTML = parolaLunga;
+      } else {
+        parolaLunga = 'Le parole sono lunghe uguali';
+        document.getElementById('risultatoSnack1').innerHTML = parolaLunga;
+      }
+
+  }
+)
+
+                // >>>>>>>>> ESERCIZIO SENZA ADDEVEVENTLISTER  <<<<<<<<<<<<
+                //
+                // var parola1 = prompt('Inserisci parola uno').trim();
+                // console.log(parola1);
+                //
+                // var parola2 = prompt('Inserisci parola due').trim();
+                // console.log(parola2);
+                //
+                // var parolaLunga = funzioneParola(parola1, parola2);
+                //
+                // console.log(parolaLunga);
+                //
+                // function funzioneParola (par1, par2) {
+                //   if ( par1.length > par2.length ) {
+                //     return 'La parola più lunga è ' + parola1;
+                //   } else if ( par1.length < par2.length ) {
+                //     return 'La parola più lunga è ' + parola2;
+                //   } else {
+                //     return ('Le parole sono lunghe uguali');
+                //   }
+                //
+                // }
+
+// Snack 2: Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+var btnSnack2 = document.getElementById('startSnack2');
+
+btnSnack2.addEventListener('click',
+  function () {
+    var numero = document.getElementById('addNumero').value;
+    console.log('Numero Inserito: ' + numero);
+
+      var ris = '';
+      if (numero % 2 == 0) {
+        ris = numero + ' è pari';
+      } else {
+        ris = ++numero + ' ' + '"Il numero era dispari"';
+      }
+
+      var risultato = ris;
+      document.getElementById('risultatoSnack2').innerHTML = risultato;
+      console.log(risultato);
+
+  }
+)
+
+            // >>>>>>>>> ESERCIZIO SENZA ADDEVEVENTLISTER  <<<<<<<<<<<<
+            // var numero = parseInt( prompt('Inserisci un numero'));
+            // console.log('Numero Inserito: ' + numero);
+            //
+            // var risultato = oddEven();
+            // console.log(risultato);
+            //
+            // function oddEven(num) {
+            //   var ris = '';
+            //
+            //   if (numero % 2 == 0) {
+            //     ris = numero + ' è pari';
+            //   } else {
+            //     ris = numero + 1 + ' "Il numero era dispari"';
+            //   }
+            //
+            //   return ris;
+            // }
+
+
+// Snack 3: Generatore di “nomi cognomi” casuali: prendendo una lista di nomi (‘Michele’, ‘Fabio’, ‘Roberto’) e una lista di cognomi (‘Forghieri’, ‘Papagni’, ‘Marazzini’), Gatsby vuole generare una falsa lista di 5 invitati.
